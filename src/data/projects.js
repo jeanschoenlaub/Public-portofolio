e97d9a006ea1412eea3ac5aca9b46647c3da9bea
Jean personal <jeanschoen@hotmail.com>
2024-03-26 14:04:11 +0545

    changed project metadata

--- src/data/projects.js
+++ src/data/projects.js
@@ -73,7 +73,7 @@ export const projectData = [
     },
     {
         title: "Greenhouse crop model",
-        type: {1:"CAD",2:"Data"},
+        type: {1:"CAD",2:"Data",3:"Solar"},
         description: "Using Rhino CAD to model light and tomato crop growth under a semi-transparent solar panels greenhouse. Paper published in Applied Energy",
         full_text: `
         <p> As I was doing my master's in Solar Energy at the University of New South Wales (UNSW) in Sydney, I became increasingly interested in Agriphotovoltaic (APV). APV is combination of using solar panels to transform the sun's energy into electricity, as well as plants for chemical energy. Multiple synergies had already been proven in this field, but I was interested in the synergies that semi-transparent solar cells could bring to APV. </p> <div style="text-align: center;"> <iframe src="https://drive.google.com/file/d/1BlY7BPrw3y0KqKGF8WYg3WazDnGyPFAa/preview" width="640" height="480"></iframe> </div> <p> Challenge was finding the right software to model everyhting. Paper is not that interesting but the methodology was novel so we decided to publish (the idea had already been explored in 2-3 papers - but never with a modelling methodology) 
